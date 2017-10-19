d976649885d6bf7b2a74de46083f81c48c4c94c2
Kirill Beresnev <derainberk@gmail.com>
2017-10-19 19:11:48 +0300

    Changed to testnet

--- ts/utils/configs.ts
+++ ts/utils/configs.ts
@@ -2,14 +2,14 @@ import * as _ from 'lodash';
 import {Environments} from 'ts/types';
 
 const BASE_URL = window.location.origin;
-const isDevelopment = _.includes(BASE_URL, 'https://0xproject.dev:3572') ||
-                      _.includes(BASE_URL, 'https://localhost:3572') ||
-                      _.includes(BASE_URL, 'https://127.0.0.1');
+const isDevelopment = _.includes(BASE_URL, 'http://0xproject.dev:3572') ||
+                      _.includes(BASE_URL, 'http://localhost:3572') ||
+                      _.includes(BASE_URL, 'http://127.0.0.1');
 
 export const configs = {
     BASE_URL,
     ENVIRONMENT: isDevelopment ? Environments.DEVELOPMENT : Environments.PRODUCTION,
-    BACKEND_BASE_URL: isDevelopment ? 'https://localhost:3001' : 'https://website-api.0xproject.com',
+    BACKEND_BASE_URL: isDevelopment ? 'http://localhost:3001' : 'http://website-api.0xproject.com',
     symbolsOfMintableTokens: ['MKR', 'MLN', 'GNT', 'DGD', 'REP'],
     // WARNING: ZRX & WETH MUST always be default trackedTokens
     defaultTrackedTokenSymbols: ['WETH', 'ZRX'],
--- ts/utils/constants.ts
+++ ts/utils/constants.ts
@@ -16,7 +16,7 @@ export const constants = {
     ANGELLIST_URL: 'https://angel.co/0xproject/jobs',
     STAGING_DOMAIN: 'staging-0xproject.s3-website-us-east-1.amazonaws.com',
     PRODUCTION_DOMAIN: '0xproject.com',
-    DEVELOPMENT_DOMAIN: '0xproject.dev:3572',
+    DEVELOPMENT_DOMAIN: '18.194.13.39:3572',
     BIGNUMBERJS_GITHUB_URL: 'http://mikemcl.github.io/bignumber.js',
     BITLY_ACCESS_TOKEN: 'ffc4c1a31e5143848fb7c523b39f91b9b213d208',
     BITLY_ENDPOINT: 'https://api-ssl.bitly.com',
